e5a0b6e114497e22213c8339f0dffc3631d876a2
daphay <dafizo1@gmail.com>
2020-09-18 16:55:14 +0100

    Updates

---
+++ precache-manifest.becf01461efb8d8abd6f9da9fdd94d7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39fdf0bcca11cd1ea4a1",
+    "url": "/daphay/todo-app/static/js/runtime~main.012ad195.js"
+  },
+  {
+    "revision": "a1bc866485c8abdc9d66",
+    "url": "/daphay/todo-app/static/js/main.1c5b5477.chunk.js"
+  },
+  {
+    "revision": "7d99f87b2b3842096bce",
+    "url": "/daphay/todo-app/static/js/2.78a25bb2.chunk.js"
+  },
+  {
+    "revision": "a1bc866485c8abdc9d66",
+    "url": "/daphay/todo-app/static/css/main.3ffa68ec.chunk.css"
+  },
+  {
+    "revision": "5417c2e907fa2b1e16f30d246dc6d035",
+    "url": "/daphay/todo-app/index.html"
+  }
+];
